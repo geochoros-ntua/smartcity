186e3c9b2569024df9e9391f2c20d8258b4d5fb3
unknown <p.tsagkis@gmail.com>
2022-04-06 22:44:40 +0300

    final

--- src/app/shared/messages.service.ts
+++ src/app/shared/messages.service.ts
@@ -13,14 +13,14 @@ export class AppMessagesService {
   constructor(private matSnackBar: MatSnackBar) { }
 /**
  * Provide message and actions mandatory
- * You may also pass duration, horizontalPosition, verticalPosition 
+ * You may also pass duration, horizontalPosition, verticalPosition
  * @param messageOptions 
  */
   showMapMessage(messageOptions: MessageOptions ): void {
     this.matSnackBar.open(messageOptions.message, messageOptions.action, {
-      duration: 50000,
+      duration: messageOptions.duration ? messageOptions.duration : 5000,
       horizontalPosition: messageOptions.hPosition ? messageOptions.hPosition : 'center',
-      verticalPosition: messageOptions.vPosition ? messageOptions.vPosition : 'top',
+      verticalPosition: messageOptions.vPosition ? messageOptions.vPosition : 'top'
     });
   }
 
