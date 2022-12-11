da7a78fa0736123f3b9214aa329bba42e25971b6
unknown <p.tsagkis@gmail.com>
2022-12-11 21:17:23 +0200

    cache data urls

--- src/php/loadMapilaryData.php
+++ src/php/loadMapilaryData.php
@@ -1,5 +1,10 @@
 <?php
 require 'connect.php';
+$seconds_to_cache = 3600;
+$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+header("Expires: $ts");
+header("Pragma: cache");
+header("Cache-Control: max-age=$seconds_to_cache");
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 
